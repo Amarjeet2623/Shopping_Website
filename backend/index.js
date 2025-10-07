831cdfe4968d3bb70c4a81d893a595b73747b9c4
Amarjeet2623 <195775733+Amarjeet2623@users.noreply.github.com>
2025-10-07 20:22:41 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin: "*",
+    // origin : process.env.FRONTEND_URL,
     credentials : true
 }))
 app.use(express.json())
